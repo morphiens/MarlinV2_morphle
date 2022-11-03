5cae8ad3ecbd805e3d685511c7f3179aefae4027
Sanjay <Varghesemela@gmail.com>
2022-11-03 17:27:52 +0530

    [Sanjay] WIP: Config added for morpholens-240 scanner

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -134,7 +134,7 @@
 //#define BLUETOOTH
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Morpholens6"
+#define CUSTOM_MACHINE_NAME "Morpholens-240_scanner"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
@@ -972,13 +972,13 @@
 // Specify here all the endstop connectors that are connected to any endstop or probe.
 // Almost all printers will be using one per axis. Probes will use one or more of the
 // extra connectors. Leave undefined any used for non-endstop and non-probe purposes.
-#define USE_XMIN_PLUG
+// #define USE_XMIN_PLUG
 #define USE_YMIN_PLUG
 // #define USE_ZMIN_PLUG
 //#define USE_IMIN_PLUG
 //#define USE_JMIN_PLUG
 //#define USE_KMIN_PLUG
-//#define USE_XMAX_PLUG
+#define USE_XMAX_PLUG
 //#define USE_YMAX_PLUG
 #define USE_ZMAX_PLUG
 //#define USE_IMAX_PLUG
@@ -1578,7 +1578,7 @@
 
 // Direction of endstops when homing; 1=MAX, -1=MIN
 // :[-1,1]
-#define X_HOME_DIR -1
+#define X_HOME_DIR 1
 #define Y_HOME_DIR -1
 #define Z_HOME_DIR 1
 //#define I_HOME_DIR -1
@@ -1588,9 +1588,9 @@
 // @section machine
 
 // The size of the printable area
-#define X_BED_SIZE 255
-#define Y_BED_SIZE 105
-#define Z_MAX_LEN -3.75
+#define X_BED_SIZE 250
+#define Y_BED_SIZE 170
+#define Z_MAX_LEN -3.6
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
