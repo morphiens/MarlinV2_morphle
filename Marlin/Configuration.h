688cc22d470776ebdbe5ed8a81d6175d96bb5a96
Sudhanshu <sudhanshu@morphle.in>
2023-06-02 12:13:12 +0530

    Comment XMIN or XaMAX

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1098,7 +1098,7 @@
 // Almost all printers will be using one per axis. Probes will use one or more of the
 // extra connectors. Leave undefined any used for non-endstop and non-probe purposes.
 #define USE_XMIN_PLUG
-#define USE_YMIN_PLUG
+// #define USE_YMIN_PLUG
 #define USE_ZMIN_PLUG
 // #define USE_IMIN_PLUG
 //#define USE_JMIN_PLUG
@@ -1106,7 +1106,7 @@
 //#define USE_UMIN_PLUG
 //#define USE_VMIN_PLUG
 //#define USE_WMIN_PLUG
-#define USE_XMAX_PLUG
+// #define USE_XMAX_PLUG
 #define USE_YMAX_PLUG
 // #define USE_ZMAX_PLUG
 #define USE_IMAX_PLUG
